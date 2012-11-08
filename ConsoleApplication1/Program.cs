a676ebbb23c254817808e400daa0b78f8cc68639
unknown <pedrogbinfo@gmail.com>
2012-11-08 17:15:57 +0100

    sdf

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -9,7 +9,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hola");
+           
+            Console.WriteLine("hadfa");
         }
     }
 }
