81b038e974b0b314f6e7cf0926488eaa16938d0c
pedrogbinfo <pedrogbinfo@gmail.com>
2012-11-09 16:10:03 +0100

    Hola turbula 2

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
            
-            Console.WriteLine("hadfa");
+            Console.WriteLine("hola turbula 2");
         }
     }
 }
